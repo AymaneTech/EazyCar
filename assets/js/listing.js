0839bf79c348b074b322099dd3d05e43b72a79a4
aymane <elmainiaymane03@gmail.com>
2023-11-02 17:22:21 +0100

    fixing hyper links of pages

--- assets/js/listing.js
+++
@@ -1,8 +0,0 @@
-var cards = document.querySelectorAll(".card");
-var listStyle = document.querySelectorAll(".listing-style i");
-
-listStyle.forEach(element => {
-    element.addEventListner("click", function (){
-        
-    });
-});
