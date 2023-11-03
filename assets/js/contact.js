6469b190c2e069bf2076890dbee47a505eb0cb19
HamliriSanaa <144817752+HamliriSanaa@users.noreply.github.com>
2023-11-03 15:04:28 +0100

    contact finished

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,79 +1,66 @@
-// const inputs = document.querySelectorAll('example');
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const phoneInput = document.getElementById("phone");
+const messageInput = document.getElementById("message");
+const sendButton = document.getElementById("send-button");
+const valuenone = document.querySelectorAll("valuenone");
 
-// const patterns ={
-//     telephone: /^\d{10}$/,
-//     lastname: /^[a-z]{5,12}$/i,
-//     firstname: /^[a-z]{5,12}$/i,
-//     // password: /^[\w\W@-]{8,20}$/,
-//     email: /^([a-z\d.-]+)@([a-z\d-]+).([a-z]{2,8})$/i,
-// }
-
-// function validate(field, regex) {
-//     if(regex.test(field.value)) {
-//         field.className='valid';
-//     }else{
-//         field.className='invalid';
-//     }
-// }
-
-// inputs.forEach((example)=> {
-//     example.addEventListener('keyup',(e)=> {
-//         validate(e.target,patterns[e.target.attributes.name.value])
-//     });
-// });
+// const nameRegex = /^[A-Za-z\s]{5,10}( [A-Za-z\s]{5,10})?$/;
+// const nameRegex = /^[A-Za-z\s]{5,10}( [A-Za-z\s]{5,10})?$/;
+const nameRegex = /^[A-Za-z\s]{5,10} [A-Za-z\s]{5,10}$/;
+const emailRegex = /^[a-zA-Z0-9._-]+@[a-z]+\.[a-zA-Z]{2,3}$/; 
+const phoneRegex =  /^0[5-6]+[0-9]{8}$/;
+const messageRegex =  /^[A-Za-z\s]{5,}$/;
 
 
+function validateInput(input, regex) {
+  if (regex.test(input.value)) {
+    input.classList.remove("is-invalid");
+    input.classList.add("is-valid");
+  } else {
+    input.classList.remove("is-valid");
+    input.classList.add("is-invalid");
+  }
+}
 
-// function validateFullName(input) {
-//     const fullName = input.value;
-//     const errorElement = document.getElementById("nameError");
-//     if (!fullName.includes(" ")) {
-//       errorElement.textContent = "Invalid Full Name...!";
-//     } else {
-//       errorElement.textContent = "";
-//     }
-//   }
-//   function validateEmail(input) {
-//     const email = input.value;
-//     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/;
-//     const errorElement = document.getElementById("emailError");
 
-//     if (!emailRegex.test(email)) {
-//       errorElement.textContent = "Adresse e-mail invalide";
-//     } else {
-//       errorElement.textContent = "";
-//     }
-//   }
+nameInput.addEventListener("input", () => {
+  validateInput(nameInput, nameRegex);
+});
 
-//   function validatePhone(input) {
-//     const phone = input.value;
-//     const phoneRegex = /^[0-9]{10}$/; 
-//     const errorElement = document.getElementById("phoneError");
+emailInput.addEventListener("input", () => {
+  validateInput(emailInput, emailRegex);
+});
 
-//     if (!phoneRegex.test(phone)) {
-//       errorElement.textContent = "Numéro de téléphone invalide";
-//     } else {
-//       errorElement.textContent = "";
-//     }
-//   }
+phoneInput.addEventListener("input", () => {
+  validateInput(phoneInput, phoneRegex);
+});
 
-//   const btnSend = document.querySelector(".btn--send");
-//   var notyf = new Notyf({duration: 3000,
-//     position: {
-//       x: 'center',
-//       y: 'top',
-//     }});
+messageInput.addEventListener("input", () => {
+  validateInput(messageInput, messageRegex);
+});
 
-//   btnSend.addEventListener("click", function (event) {
-// event.preventDefault();
-//     const cin = document.getElementById("cin").value;
-//     const email = document.getElementById("e-mail").value;
-//     const phone = document.getElementById("phone").value;
+sendButton.addEventListener("click", () => {
 
-//     if (cin === ""  email === ""  phone === "") {
-//         notyf.error("Tous les champs doivent être remplis.");
+  if (!nameRegex.test(nameInput.value)) {
+    alert("wrong name");
+  }
+  else if (!emailRegex.test(emailInput.value)) {
+    alert("wrong email");
+  }
 
-//     }else{
-//         notyf.success("Send...");
-//     }
-//   });
+  else if (!phoneRegex.test(phoneInput.value)) {
+    alert("wrong phone number");
+  }
+
+  else if (!messageRegex.test(messageInput.value)) {
+    alert("somthing wrong in message");
+  }
+
+   else {
+    alert("THANK YOU FOR SUBMITING");
+    nameInput.value = '';
+    emailInput.value = '';
+    phoneInput.value = '';
+  }
+});
