e7e0c86240b6bbf7a28cafd62ce5deddf3fb3824
anouar <anouar.ab95@gmail.com>
2023-11-04 22:30:22 +0100

    is corected code pannier

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
-const localData = (JSON.parse(localStorage.getItem("carsData")));
-const panelItems = (JSON.parse(localStorage.getItem("PanelData")));
+const localData = JSON.parse(localStorage.getItem("carsData"));
+const panelItems = JSON.parse(localStorage.getItem("PanelData")) ;
 const cardsParent = document.querySelector("#cardsParent");
 const pageNum = document.querySelectorAll(".page-num");
 const allcards = document.querySelector(".cards");
@@ -7,46 +7,49 @@ const addCart = document.querySelectorAll("#addCart");
 
 const panierContent = document.querySelector(".panier-content");
 
-
-var panelArr = [];
 var panelCounter = document.getElementById("panelCounter");
+var panelArr = panelItems || [];
+
 const filterItem = document.querySelectorAll(".filter-btn");
 
 // ==============================================================
-//                         insert cards 
+// Insert cards
 for (let i = 0; i < localData.length; i++) {
-  const html = `<div style="" class="card col-md-3 col-lg-4 w-100 ">
-    <img class="card-img-top" src=${localData[i].image} alt="Image">
-<div class="card-body">
-<h5 class="card-title">${localData[i].name}</h5>
-    <p class="card-text">${localData[i].description}</p>
-    <div class="details d-flex align-items-center justify-content-between">
-    <p class="card-price bold m-0 m-0">
-    </p>
-    <button class="button addCard">Rent Now</button>
+  const html = `<div class="card col-md-3 col-lg-4 w-100">
+    <img class="card-img-top" src="${localData[i].image}" alt="Image">
+    <div class="card-body">
+      <h5 class="card-title">${localData[i].name}</h5>
+      <p class="card-text">${localData[i].description}</p>
+      <div class="details d-flex align-items-center justify-content-between">
+        <p class="card-price bold m-0 m-0">
+        </p>
+        <button class="button addCard">Rent Now</button>
+      </div>
     </div>
-    </div>
-</div>`;
+  </div>`;
 
   cardsParent.insertAdjacentHTML("afterbegin", html);
 }
 
 // ==============================================================
-//                  pagination
+// Pagination
 var card = document.querySelectorAll(".card");
 hide(0, 6);
+
 function hide(start, stop) {
   card.forEach((element, index) => {
     if (start <= index && index < stop) {
-      element.style.display = "block"
+      element.style.display = "block";
     } else {
-      element.style.display = "none"
+      element.style.display = "none";
     }
   });
 }
+
 // ==============================================================
-//                Filter
+// Filter
 filterFunction();
+
 function filterFunction() {
   filterItem.forEach(item => {
     item.addEventListener("click", function () {
@@ -65,18 +68,21 @@ function filterFunction() {
     });
   });
 }
+
 // ==============================================================
-//        add corder to local storage
+// Add car to local storage
+function updatePanelCounter() {
+  panelCounter.textContent = panelArr.length;
+}
 
 var addCard = document.querySelectorAll(".addCard");
 
 addCard.forEach((element, index) => {
   element.addEventListener("click", () => {
-
     panelArr.push(localData[index]);
-    
-    panelCounter.textContent++;
     localStorage.setItem("PanelData", JSON.stringify(panelArr));
+    updatePanelCounter();
+    addCartPanel();
   });
 });
 
@@ -86,11 +92,10 @@ openCartButton.addEventListener("click", function () {
   const cartModal = new bootstrap.Modal(document.getElementById("cartModal"));
   cartModal.show();
 });
-addCartPanel();
 
 function addCartPanel() {
   panierContent.innerHTML = '';
-  panelItems.forEach((panelItem) => {
+  panelArr.forEach((panelItem) => {
     const panelItemHTML = `
       <div class="list-group-item panier-item d-flex justify-content-space-between w-100">
         <img class="w-25 h-25" src="${panelItem.image}" alt="">
@@ -104,5 +109,5 @@ function addCartPanel() {
   });
 }
 
-
-
+updatePanelCounter();
+addCartPanel();
